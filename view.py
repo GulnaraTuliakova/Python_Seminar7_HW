e966fe489abaccf5ef43f735227fbf09a70f301f
GulnaraTuliakova <103924899+GulnaraTuliakova@users.noreply.github.com>
2022-08-04 11:58:05 +0500

    Update view.py
    Add changes in view

--- view.py
+++ view.py
@@ -1,22 +1,6 @@
-# import model as m
-# import controller as c
 
 def input_data (text):
     return input (text)
 
 def view_data (text):
     print (text)
-
-
-# def surname_view(data): 
-#     model = m.surname_model(input('Введите фамилию: '))
-#     c.surname_logger = model
-#     return model
-# def name_view(data): 
-#     data = m.name_model(input('Введите имя: '))
-#     c.name_logger = data
-#     return data
-# def second_name_view(data): 
-#     data = m.second_name_model(input('Введите имя: '))
-#     c.second_name_logger = data
-#     return data
